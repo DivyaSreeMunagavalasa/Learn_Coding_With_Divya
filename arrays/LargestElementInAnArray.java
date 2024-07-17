939b1fcd4551e44990ad3203f07b17dbf1e4c674
DivyaSreeMunagavalasa <divya.sree@datakaveri.org>
2024-07-17 17:11:58 +0530

    Adding array problems - Adding 1st array problem, largest element in an array

---
+++ arrays/LargestElementInAnArray.java
@@ -0,0 +1,33 @@
+package arrays;
+
+import java.util.Scanner;
+public class LargestElementInAnArray {
+    public static int[] getArray(int n){
+        Scanner s = new Scanner(System.in);
+        int [] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = s.nextInt();
+        }
+        return arr;
+    }
+
+    public static int getLargestElement(int [] arr){
+        int largest = arr[0];
+        for(int i = 1 ; i < arr.length ; i++){
+            if(arr[i] > largest){
+                largest = arr[i];
+            }
+        }
+        return largest;
+    }
+
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number of elements in an array: ");
+        int n = s.nextInt();
+        int [] arr ;
+        arr = getArray(n);
+        int largest = getLargestElement(arr);
+        System.out.println("The largest element in an array is: " + largest);
+    }
+}
