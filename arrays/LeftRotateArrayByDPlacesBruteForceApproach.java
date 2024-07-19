57716d03adc7875876f471dadbcf330f97b819d9
DivyaSreeMunagavalasa <divya.sree@datakaveri.org>
2024-07-19 21:34:53 +0530

    Adding problem-7 : Rotate array to left by d elements- both brute force and optimal approaches

---
+++ arrays/LeftRotateArrayByDPlacesBruteForceApproach.java
@@ -0,0 +1,35 @@
+package arrays;
+import java.util.Scanner;
+
+import static arrays.CreateAndPrintArray.*;
+
+public class LeftRotateArrayByDPlacesBruteForceApproach {
+    // This is the Brute Force Approach to rotate the array to left by d places
+    public static void rotateArrayLeftByDPlaces(int [] arr, int d){
+        int n = arr.length;
+        d = d % n;
+        int [] temp = new int[d];
+        for(int i = 0 ; i < d ; i++){
+            temp[i] = arr[i];
+        }
+        for(int i = d ; i < n ; i++){
+            arr[i-d] = arr[i];
+        }
+        for(int i = n-d ; i < n ; i++){
+            arr[i] = temp[i-(n-d)];
+        }
+    }
+    public static void main(String [] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number of places to rotate the array to left : ");
+        int d = s.nextInt();
+        System.out.println("Enter the number of elements in an array : ");
+        int n = s.nextInt();
+        int [] arr = getArray(n);
+        System.out.println("The original array is: ");
+        printArray(arr);
+        rotateArrayLeftByDPlaces(arr,d);
+        System.out.println("The left rotated array by one place is: ");
+        printArray(arr);
+    }
+}
---
+++ arrays/LeftRotateArrayByDPlacesOptimalApproach.java
@@ -0,0 +1,36 @@
+package arrays;
+import java.util.Scanner;
+
+import static arrays.CreateAndPrintArray.*;
+
+public class LeftRotateArrayByDPlacesOptimalApproach {
+    public static void rotateArrayLeftByDPlaces(int[] arr, int d){
+        int n = arr.length;
+        d = d % n;
+        reverse(arr,0,d-1);
+        reverse(arr, d , n-1);
+        reverse(arr,0,n-1);
+    }
+    public static void reverse (int [] arr, int start, int end){
+        while(start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number of places to rotate the array to the left: ");
+        int d = s.nextInt();
+        System.out.println("Enter the number of elements in an array: ");
+        int n = s.nextInt();
+        int [] arr = getArray(n);
+        System.out.print("The original array is: ");
+        printArray(arr);
+        rotateArrayLeftByDPlaces(arr,d);
+        System.out.printf("The array after rotating left by %d places is: ", d);
+        printArray(arr);
+    }
+}
