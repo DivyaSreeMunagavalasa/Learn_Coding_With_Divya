9348fb8042551623fea671f44c2dac96c14e9c1c
DivyaSreeMunagavalasa <divya.sree@datakaveri.org>
2024-07-19 21:35:45 +0530

    Adding problem-8 : Rotate array to right by d elements- both brute force and optimal approaches

---
+++ arrays/RightRotateArrayByDPlacesBruteForceApproach.java
@@ -0,0 +1,40 @@
+package arrays;
+
+import java.util.Scanner;
+
+import static arrays.CreateAndPrintArray.getArray;
+import static arrays.CreateAndPrintArray.printArray;
+
+public class RightRotateArrayByDPlacesBruteForceApproach {
+    // This is the Brute Force Approach to rotate the array to right by d places
+    public static void rotateArrayRightByDPlaces(int [] nums, int k){
+        int n = nums.length;
+        k = k % n;
+        int [] temp = new int[k];
+        // Copy the last k elements to temp
+        for (int i = 0; i < k; i++) {
+            temp[i] = nums[n - k + i];
+        }
+        // Shift the rest of the array to the right by k positions
+        for (int i = n - 1; i >= k; i--) {
+            nums[i] = nums[i - k];
+        }
+        // Copy the elements from temp to the beginning of the array
+        for (int i = 0; i < k; i++) {
+            nums[i] = temp[i];
+        }
+    }
+    public static void main(String [] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number of places to rotate the array to right : ");
+        int d = s.nextInt();
+        System.out.println("Enter the number of elements in an array : ");
+        int n = s.nextInt();
+        int [] arr = getArray(n);
+        System.out.println("The original array is: ");
+        printArray(arr);
+        rotateArrayRightByDPlaces(arr,d);
+        System.out.println("The left rotated array by one place is: ");
+        printArray(arr);
+    }
+}
---
+++ arrays/RightRotateArrayByDPlacesOptimalApproach.java
@@ -0,0 +1,38 @@
+package arrays;
+
+import java.util.Scanner;
+
+import static arrays.CreateAndPrintArray.getArray;
+import static arrays.CreateAndPrintArray.printArray;
+
+public class RightRotateArrayByDPlacesOptimalApproach {
+    public static void rotateArrayRightByDPlaces(int[] arr, int d){
+        int n = arr.length;
+        d = d % n;
+        reverse(arr,0,n-1);
+        reverse(arr, 0 , d-1);
+        reverse(arr,d,n-1);
+    }
+    public static void reverse (int [] arr, int start, int end){
+        while(start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number of places to rotate the array to the right: ");
+        int d = s.nextInt();
+        System.out.println("Enter the number of elements in an array: ");
+        int n = s.nextInt();
+        int [] arr = getArray(n);
+        System.out.print("The original array is: ");
+        printArray(arr);
+        rotateArrayRightByDPlaces(arr,d);
+        System.out.printf("The array after rotating right by %d places is: ", d);
+        printArray(arr);
+    }
+}
